49b5e094ce7b6f086de4d6b656f6006e51278602
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2022-11-10 20:45:06 +0530

    krishan day-3

--- stayfit/src/Pages/signup/SignupActivity.jsx
+++ stayfit/src/Pages/signup/SignupActivity.jsx
@@ -8,14 +8,14 @@ const SignupActivity = () => {
 	const navigate = useNavigate();
 
 	const handleClick = () => {
-		navigate("/signupTall");
+		navigate("/signupBirth");
 	};
 	return (
 		<>
 			<SignupNavbar />
 			<Box className="signupWrapper">
-				<Box className="insideBox" h="fit-content" p="20px">
-					<Heading fontSize="25px">
+				<Box className="insideBox" h="fit-content" p="20px" w='40%'>
+					<Heading fontSize="22px" mt='2.5rem'>
 						What is your baseline activity level?
 					</Heading>
 					<Text color="gray" mb="20px">
@@ -25,17 +25,21 @@ const SignupActivity = () => {
 					<Button
 						colorScheme="black"
 						variant="outline"
-						borderColor="#0066EE"
 						w="400px"
-						my="20px"
+						my="10px"
 						h="fit-content"
+						borderRadius={'0'}
+						borderColor='grey'
+						color='grey'
+						mb="10px"
+						_hover={{color:'blue',borderColor:'blue',border:'2px'}}	
 						onClick={handleClick}
 					>
 						<Box p="5px" textAlign="left" w="100%">
-							<Text fontSize="18px" color="#0066EE">
+							<Text fontSize="16px" fontWeight={'400'} color="black" _hover={{color:'blue'}}>
 								Not Very Active
 							</Text>
-							<Text fontSize="14px" color="#909090">
+							<Text fontSize="14px" color="#909090" fontWeight={'400'} lineHeight='1.5rem'>
 								Spends most of the day sitting
 							</Text>
 							<Text fontSize="14px" color="#909090">
@@ -47,17 +51,20 @@ const SignupActivity = () => {
 					<Button
 						colorScheme="black"
 						variant="outline"
-						borderColor="#0066EE"
+						borderRadius={'0'}
+						borderColor='grey'
+						color='grey'
+						mb="10px"
+						_hover={{color:'blue',borderColor:'blue',border:'2px'}}
 						w="400px"
-						mb="20px"
 						h="fit-content"
 						onClick={handleClick}
 					>
 						<Box p="5px" textAlign="left">
-							<Text fontSize="18px" color="#0066EE">
+							<Text fontSize="16px" fontWeight={'400'} color="black" _hover={{color:'blue'}}>
 								Lightly Active
 							</Text>
-							<Text fontSize="14px" color="#909090">
+							<Text fontSize="14px" color="#909090" fontWeight={'400'} lineHeight='1.5rem'>
 								Spends a considerably good part of the day on your feet
 							</Text>
 							<Text fontSize="14px" color="#909090">
@@ -69,17 +76,21 @@ const SignupActivity = () => {
 					<Button
 						colorScheme="black"
 						variant="outline"
-						borderColor="#0066EE"
 						w="400px"
-						mb="20px"
+						mb="10px"
+						borderRadius={'0'}
+						borderColor='grey'
+						color='grey'
+						_hover={{color:'blue',borderColor:'blue',border:'2px'}}
+						
 						h="fit-content"
 						onClick={handleClick}
 					>
 						<Box p="5px" textAlign="left">
-							<Text fontSize="18px" color="#0066EE">
+							<Text fontSize="16px" fontWeight={'400'} color="black" _hover={{color:'blue'}}>
 								Active
 							</Text>
-							<Text fontSize="14px" color="#909090">
+							<Text fontSize="14px" color="#909090" fontWeight={'400'} lineHeight='1.5rem'>
 								Spend a good part of the day doing some physical activity
 							</Text>
 							<Text fontSize="14px" color="#909090">
@@ -90,18 +101,22 @@ const SignupActivity = () => {
 
 					<Button
 						colorScheme="black"
-						borderColor="#0066EE"
+						
 						variant="outline"
 						w="400px"
 						mb="20px"
+						borderRadius={'0'}
+						borderColor='grey'
+						color='grey'
+						_hover={{color:'blue',borderColor:'blue',border:'2px'}}
 						h="fit-content"
 						onClick={handleClick}
 					>
-						<Box p="5px" textAlign="left">
-							<Text fontSize="18px" color="#0066EE">
+						<Box p="5px" textAlign="left" >
+							<Text fontSize="16px" fontWeight={'400'} color="black" _hover={{color:'blue'}}>
 								Very Active
 							</Text>
-							<Text fontSize="14px" color="#909090">
+							<Text fontSize="14px" color="#909090" fontWeight={'400'} lineHeight='1.5rem'>
 								Spend a good part of the day doing heavy physical activity
 							</Text>
 							<Text fontSize="14px" color="#909090">
